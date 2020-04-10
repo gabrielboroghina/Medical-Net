d143e48d9e6f8583e1534e70703ac78f0554f130
Gabriel Boroghina <gabrielboroghina@outlook.com>
2020-04-10 12:36:21 +0300

    Initialization of React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {DocumentCardImageExample} from "./card";
+
+function App() {
+    return (
+        <div className="App">
+            <DocumentCardImageExample/>
+        </div>
+    );
+}
+
+export default App;
